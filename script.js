5bd1d9e0f100f9227ef586a1659977b53dfe40a1
Moksleivis <Moksleivis@kitm.local>
2023-03-31 12:20:11 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let vardas = "Kestutis";
+let amzius = 30;
+let grupe = "js1s";
+
+console.log("Mano varda: "+vardas);
+console.log(`mano amzius yra: ${amzius}`);
+console.log(`mano paskaitu grupe: ${grupe}`)
